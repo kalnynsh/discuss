c7cd064199f3bc5c413e3dedb96d85e4af7348b9
kalnynsh <kda869@yandex.ru>
2017-03-28 19:12:14 +0300

    In /urls add to urlpatterns urls name: home, login, logout, user-registration.

--- discuss/urls.py
+++ discuss/urls.py
@@ -17,11 +17,19 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
+from django.views.generic import TemplateView
+
+from accounts.views import UserRegistrationView
+from links.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', HomeView.as_view(), name='home'),
+
+    url(r'^login/$', login, kwargs={'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', logout, kwargs={'next_page': '/login/'}, name='logout'),
+    url(r'^register/$', UserRegistrationView.as_view(), name='user-registration'),
 
 
 ]
--- links/views.py
+++ links/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import TemplateView
+from django.views.generic import View
+from django.utils import timezone
 
-# Create your views here.
+from links.models import Link
+from links.forms import CommentModelForm
+from links.models import Comment
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
