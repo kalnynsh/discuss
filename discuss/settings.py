e1afbbea4f390cdd3be91ac2c6f1f73e40d842cb
kalnynsh <kda869@yandex.ru>
2017-03-28 18:16:04 +0300

    In /discuss/settings add INSTALLED_APPS 'accounts', MIDDLEWARE ..SessionAuthenticationMiddleware. In /templates create base.html.

--- discuss/settings.py
+++ discuss/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -45,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
