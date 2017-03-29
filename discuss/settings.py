623eb4d6863f5f50e500a534ead873ad5d1cfa6f
kalnynsh <kda869@yandex.ru>
2017-03-29 19:24:20 +0300

    In /setting add STATIC_DIRS, STATICFILES_DIR.

--- discuss/settings.py
+++ discuss/settings.py
@@ -139,6 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = [STATIC_DIR, ]
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
