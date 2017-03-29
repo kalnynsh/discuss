1d07d33794628046eb51604f329b5f10dccdedba
kalnynsh <kda869@yandex.ru>
2017-03-29 18:45:45 +0300

    In /links/views create class NewSubmissionView(CreateView) with methods: dispatch, form_valid, get_success_url.

--- links/views.py
+++ links/views.py
@@ -13,5 +13,29 @@ from links.forms import CommentModelForm
 from links.models import Comment
 
 
+class NewSubmissionView(CreateView):
+    model = Link
+    fields = (
+        'title', 'url',
+    )
+
+    template_name = 'new_submission.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(NewSubmissionView, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        new_link = form.save(commit=False)
+        new_link.submitted_by = self.request.user
+        new_link.save()
+
+        self.object = new_link
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('home')
+
+
 class HomeView(TemplateView):
     template_name = 'home.html'
