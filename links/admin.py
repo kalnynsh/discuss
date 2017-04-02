dacff637cfadd58df875b2f3596551e8ea1e99b1
kalnynsh <kda869@yandex.ru>
2017-04-02 18:53:15 +0300

    In /links/admin register LinkAdmin, CommentAdmin.

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from links.models import Link, Comment
 
-# Register your models here.
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'submitted_by', 'submitted_on')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'commented_on', 'in_reply_to',
+                    'commented_by', 'created_on')
+
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(Comment, CommentAdmin)
