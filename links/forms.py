f8c51f7ac2a563574f5abfbe24ab1767d5440a22
kalnynsh <kda869@yandex.ru>
2017-04-04 16:58:36 +0300

    In /links/forms get back ReCaptcha.

--- links/forms.py
+++ links/forms.py
@@ -7,7 +7,7 @@ from links.models import Comment
 class CommentModelForm(forms.ModelForm):
     link_pk = forms.IntegerField(widget=forms.HiddenInput)
     parent_comment_pk = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    # captcha = ReCaptchaField()
+    captcha = ReCaptchaField()
 
     class Meta:
         model = Comment
